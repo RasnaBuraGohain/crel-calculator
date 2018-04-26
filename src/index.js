0f24301826c2d837972ad99932d318a8ffcb28f0
Rasna Bura Gohain <rasna.maarten@gmail.com>
2018-04-26 13:55:08 +0200

    added css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const crel = require('crel')
 const mobx = require('mobx')
 const render = require('./render.js')
+require('./index.css')
 
 const viewport = crel('div', crel('div'))
 document.body.appendChild(viewport)
 
 const state = mobx.observable({
-  input: '1+8*',
+  input: '' ,
   output: '',
 })
 
--- src/render.js
+++ src/render.js
@@ -4,6 +4,7 @@ function render(state) {
   function renderScreen() {
     return crel('input', {
       value: state.input,
+      disabled: true,
     })
   }
 
@@ -19,27 +20,26 @@ function render(state) {
     return button
   }
 
+  function renderButtons() {
+    const buttons = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].map(val => renderButton(val))
+    const operations = ['+', '-', '*', '/'].map(val => renderButton(val))
+    return crel('div', buttons, operations, renderEvalButton(), renderClearButton())
+  }
+
+    function renderEvalButton() {
+    const button = crel('button', '=')
+    button.addEventListener('click', function (event) {
+      const result = eval(state.input)
+      state.input = result
+    })
+    return button
+  }
   function renderClearButton() {
-    const button = crel('button', 'clr')
+    const button = crel('button',{class:"clr"}, 'C')
     button.addEventListener('click', function (event) {
       state.input = ''
     })
     return button
-  }
-
-  function renderEvalButton() {
-    const button = crel('button', '=')
-    button.addEventListener('click', function (event) {
-      const result = eval(state.input)
-      state.output = result
-    })
-    return button
-  }
-
-  function renderButtons() {
-    const buttons = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].map(val => renderButton(val))
-    const operations = ['+', '-', '*', '/'].map(val => renderButton(val))
-    return crel('div', buttons, operations, renderClearButton(), renderEvalButton())
   }
 
   return crel('div',
